d8b277ee24f8609b0a7fb97fbf1c3780673ef313
stevelomax1 <steven.lomax1@gmail.comm>
2023-12-27 22:45:14 -0500

    first test run of js file

---
+++ test-folder/test-js.js
@@ -0,0 +1,9 @@
+//this is test code
+
+var x = 1;
+var y = "1";
+var isAlwaysZero = x * 0;
+
+console.log("what is x", x);
+console.log('what is the type of y?', typeof y);
+console.log("is this zero?", isAlwaysZero);
